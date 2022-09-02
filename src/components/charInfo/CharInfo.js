f3ffb12011729706efb9873a80c52969db2aae2d
Nessunskill <fazly.iura@gmail.com>
2022-09-02 18:25:39 +0300

    CharInfo from classes to hooks

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,98 +1,102 @@
-import { Component } from 'react/cjs/react.production.min';
+import {useState, useEffect} from 'react';
 import MarvelServices from '../../services/MarvelServices';
 import Spinner from '../spinner/Spinner';
 import Skeleton from '../skeleton/Skeleton';
 
 import './charInfo.scss';
 
-class CharInfo extends Component {
-    state = {
-        skeleton: true,
-        loading: false
-    }
-
-    marvelServices = new MarvelServices();
+const CharInfo = (props) => {
+    const [skeleton, setSkeleton] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [name, setName] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [thumbnail, setThumbnail] = useState(null);
+    const [wiki, setWiki] = useState(null);
+    const [homepage, setHomepage] = useState(null);
+    const [comics, setComics] = useState(null);
+    const [error, setError] = useState(false);
+    const marvelServices = new MarvelServices();
 
     // Component hooks
-    componentDidUpdate = (prevProps) => {
-        if (prevProps.selectedCharacter !== this.props.selectedCharacter) {
-            this.getCharacter(this.props.selectedCharacter);
-        }
-    }
+
 
     // Getting character
-    getCharacter = (id) => {
-        this.setState({loading: true, skeleton: false});
+    const getCharacter = (id) => {
+        if (!error) {
+            setLoading(true);
+            setSkeleton(false);
+        }
         
-        this.marvelServices
+        marvelServices
             .getCharacterById(id)
             .then((character) => {
-                this.setState(() => {                  
-                    return {
-                        name: character.name,
-                        description: character.description,
-                        thumbnail: character.thumbnail,
-                        wiki: character.wiki,
-                        homepage: character.homepage,
-                        comics: character.comics,
-                        loading: false
-                    }
-                });
+                setName(character.name);
+                setDescription(character.description);
+                setThumbnail(character.thumbnail);
+                setWiki(character.wiki);
+                setHomepage(character.homepage);
+                setComics(character.comics);
+                setLoading(false);
+            })
+            .catch(() => {
+                setError(true);
             });
     }
 
-    render() {
-        const {name, description, wiki, homepage, thumbnail, comics, skeleton, loading} = this.state;
+    useEffect(() => {
+        if (props.selectedCharacter) {
+            getCharacter(props.selectedCharacter);
+        }
+    }, [props.selectedCharacter]);
 
-        let objectFitClass = null;
-        thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? objectFitClass = 'contain' : objectFitClass = 'cover';
-        
-        return (
-            <div className="char__info">
-                {skeleton ? <Skeleton/> : null}
-                {loading ? <div style={{display: 'flex', width: '100%', justifyContent: 'center'}}><Spinner/></div> : null}
+    let objectFitClass = null;
+    thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? objectFitClass = 'contain' : objectFitClass = 'cover';
+    
+    return (
+        <div className="char__info">
+            {skeleton ? <Skeleton/> : null}
+            {loading ? <div style={{display: 'flex', width: '100%', justifyContent: 'center'}}><Spinner/></div> : null}
 
+            {
+                !skeleton && !loading ?
+                    <>
+                        <div className="char__basics">
+                        <img src={thumbnail} alt={name} style={{objectFit: objectFitClass}}/>
+                        <div>
+                            <div className="char__info-name">{name}</div>
+                            <div className="char__btns">
+                                <a href={homepage} className="button button__main">
+                                    <div className="inner">homepage</div>
+                                </a>
+                                <a href={wiki} className="button button__secondary">
+                                    <div className="inner">Wiki</div>
+                                </a>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="char__descr">{description}</div>
+                        <div className="char__comics">{comics.length > 0 ? "Comics:" : "No comics found for this character"}</div>
+                    </> : null
+            }
+            <ul className="char__comics-list">
                 {
-                    !skeleton && !loading ?
-                        <>
-                            <div className="char__basics">
-                            <img src={thumbnail} alt={name} style={{objectFit: objectFitClass}}/>
-                            <div>
-                                <div className="char__info-name">{name}</div>
-                                <div className="char__btns">
-                                    <a href={homepage} className="button button__main">
-                                        <div className="inner">homepage</div>
-                                    </a>
-                                    <a href={wiki} className="button button__secondary">
-                                        <div className="inner">Wiki</div>
-                                    </a>
-                                </div>
-                            </div>
-                            </div>
-                            <div className="char__descr">{description}</div>
-                            <div className="char__comics">{comics.length > 0 ? "Comics:" : "No comics found for this character"}</div>
-                        </> : null
-                }
-                <ul className="char__comics-list">
-                    {
-                        !loading && !skeleton ?
-                            comics.map((item, index) => {
-                                if (index > 9) return;
+                    !loading && !skeleton && !error ?
+                        comics.map((item, index) => {
+                            if (index > 9) return;
 
-                                return(
-                                    <li 
-                                        key={index}
-                                        className="char__comics-item">
-                                        {item.name}
-                                    </li>                                    
-                                );
-                            }) 
-                        : null
-                    }
-                </ul>
-            </div>
-        );
-    }
+                            return(
+                                <li 
+                                    key={index}
+                                    className="char__comics-item">
+                                    {item.name}
+                                </li>                                    
+                            );
+                        }) 
+                    : null
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default CharInfo;
